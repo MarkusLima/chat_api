6b033b29dfa0c50c3c3a4b70ec148d69ea84b49e
Markus Lima <markusmak@gmail.com>
2021-07-10 15:05:41 -0300

    ok

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.set('views', path.join(__dirname, 'public'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
-app.use('/', (req, res)=>{
-    res.render('index.html');
+app.get('/', (req, res)=>{
+    res.sendFile('index.html');
 });
 
 let messages = [];
